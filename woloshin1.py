2c773d81873a69f826c551af41934556c1853cb6
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-01-21 14:51:35 -0500

    Updated from Brython Server: 1/21/2016 2:51:35 PM

--- woloshin1.py
+++ woloshin1.py
@@ -1,1 +1,12 @@
+"""
+helloworld.py
+Author: <Eli Woloshin>
+Credit: <none>
 
+Assignment:
+
+Write and submit a Python program that prints the following:
+
+Hello, world!
+"""
+print("Hello, world!")
